6ed4a6df61032cbc87db4aef419e2e29d3e548c3
newcomt-lab <newcometcto729@gmail.com>
2021-12-28 22:26:52 +0900

    added submit page

--- src/views/new-page/index.js
+++ src/views/new-page/index.js
@@ -1,15 +1,185 @@
-// material-ui
-import { Typography } from '@mui/material';
+import * as React from 'react';
+
+import {
+    Grid,
+    TextField,
+    Select,
+    MenuItem,
+    Switch,
+    FormGroup,
+    FormControlLabel,
+    Checkbox,
+    FormControl,
+    FormLabel,
+    Button
+} from '@mui/material';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const NewPage = () => (
-    <MainCard title="Sample Card">
-        <Typography variant="body2">This is new page.</Typography>
-    </MainCard>
-);
+export default function NewPage() {
+    const [value, setValue] = React.useState(null);
+    const [age, setAge] = React.useState('');
+    const [flag, setFlag] = React.useState(false);
 
-export default NewPage;
+    const handleChange = (event) => {
+      setAge(event.target.value);
+    };
+  
+    return (
+        <MainCard title="Add New Vesting">
+            <Grid container rowSpacing={2}>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={12}>
+                            <TextField id="recipient_address" fullWidth label="Recipient Address" variant="outlined" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={6}>
+                            <TextField id="recipient_name" fullWidth label="Recipient Name" variant="outlined" />
+                        </Grid>
+                        <Grid item sm={6}>
+                            <TextField id="recipient_cohort" fullWidth label="Cohort" variant="outlined" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={6}>
+                            <TextField id="amount" fullWidth label="Amount" variant="outlined" />
+                        </Grid>
+                        <Grid item sm={6}>
+                            <TextField id="strike_amount" fullWidth label="Strike Amount" variant="outlined" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={6}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DatePicker
+                                    label="Start Date"
+                                    value={value}
+                                    onChange={(newValue) => {
+                                    setValue(newValue);
+                                    }}
+                                    renderInput={(params) => 
+                                        <TextField id="start_date" fullWidth {...params} />
+                                    }
+                                />
+                            </LocalizationProvider>
+                        </Grid>
+                        <Grid item sm={6}>
+                            <TextField id="period" fullWidth label="Period(Months)" variant="outlined" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={4}>
+                            <TextField id="release_frequency" fullWidth label="Release Frequency" variant="outlined" />
+                        </Grid>
+                        <Grid item sm={2}>
+                            <FormControl fullWidth>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={age}
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={1}>second</MenuItem>
+                                    <MenuItem value={60}>minute</MenuItem>
+                                    <MenuItem value={3600}>hour</MenuItem>
+                                    <MenuItem value={86400}>day</MenuItem>
+                                    <MenuItem value={604800}>week</MenuItem>
+                                    <MenuItem value={2592000}>month</MenuItem>
+                                    <MenuItem value={31536000}>year</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    {/* <Grid container sm={12} columnSpacing={2}>
+                    </Grid> */}
+                        <Switch {...{ inputProps: { 'aria-label': 'Switch demo' } }} />
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={4}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DatePicker
+                                    label="Cliff Date"
+                                    value={value}
+                                    onChange={(newValue) => {
+                                    setValue(newValue);
+                                    }}
+                                    renderInput={(params) => 
+                                        <TextField id="cliff_date" fullWidth {...params} />
+                                    }
+                                />
+                            </LocalizationProvider>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={4}>
+                            <FormControl component="fieldset" variant="standard">
+                                <FormLabel component="legend">Who can transfer?</FormLabel>
+                                    <FormGroup row={true}>
+                                        <FormControlLabel
+                                            control={
+                                            <Checkbox checked={flag} onChange={() => setFlag(!flag)} name="flag" />
+                                            }
+                                            label="Sender"
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                            <Checkbox checked={flag} onChange={() => setFlag(!flag)} name="flag" />
+                                            }
+                                            label="Sender"
+                                        />
+                                    </FormGroup>
+                            </FormControl>
+                        </Grid>
+                        <Grid item sm={4}>
+                            <FormControl component="fieldset" variant="standard">
+                                <FormLabel component="legend">Who can cancel?</FormLabel>
+                                    <FormGroup row={true}>
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox checked={flag} onChange={() => setFlag(!flag)} name="flag" />
+                                            }
+                                            label="Sender"
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox checked={flag} onChange={() => setFlag(!flag)} name="flag" />
+                                            }
+                                            label="Sender"
+                                        />
+                                    </FormGroup>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item sm={12}>
+                    <Grid container sm={8} columnSpacing={2}>
+                        <Grid item sm={4}>
+                            <Button variant="contained">Add New</Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </MainCard>
+    );
+}
--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -1,10 +1,16 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Button,
+    Stack
+} from '@mui/material';
+import MainCard from 'ui-component/cards/MainCard';
 
 function createData(name, cohort, amount, vested, start, end, cliffDate, status) {
     return { name, cohort, amount, vested, start, end, cliffDate, status };
@@ -19,35 +25,47 @@ const rows = [
 
 export default function BasicTable() {
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="center">Cohort</TableCell>
-                        <TableCell align="center">Amount</TableCell>
-                        <TableCell align="center">Vested</TableCell>
-                        <TableCell align="center">Start</TableCell>
-                        <TableCell align="center">End</TableCell>
-                        <TableCell align="center">Cliff Date</TableCell>
-                        <TableCell align="center">Status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell align="center">{row.cohort}</TableCell>
-                            <TableCell align="center">{row.amount}</TableCell>
-                            <TableCell align="center">{row.vested}</TableCell>
-                            <TableCell align="center">{row.start}</TableCell>
-                            <TableCell align="center">{row.end}</TableCell>
-                            <TableCell align="center">{row.cliffDate}</TableCell>
-                            <TableCell align="center">{row.status}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <MainCard>
+            <Grid>
+                <Grid sx={{ mb : 2 }}>
+                    <Stack spacing={2} direction="row">
+                        <Button variant="contained">Add New</Button>
+                        <Button variant="contained" color="warning">Deactivate</Button>
+                    </Stack>
+                </Grid>
+                <Grid>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell align="center">Cohort</TableCell>
+                                    <TableCell align="center">Amount</TableCell>
+                                    <TableCell align="center">Vested</TableCell>
+                                    <TableCell align="center">Start</TableCell>
+                                    <TableCell align="center">End</TableCell>
+                                    <TableCell align="center">Cliff Date</TableCell>
+                                    <TableCell align="center">Status</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow>
+                                        <TableCell>{row.name}</TableCell>
+                                        <TableCell align="center">{row.cohort}</TableCell>
+                                        <TableCell align="center">{row.amount}</TableCell>
+                                        <TableCell align="center">{row.vested}</TableCell>
+                                        <TableCell align="center">{row.start}</TableCell>
+                                        <TableCell align="center">{row.end}</TableCell>
+                                        <TableCell align="center">{row.cliffDate}</TableCell>
+                                        <TableCell align="center">{row.status}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+        </MainCard>
     );
 }
